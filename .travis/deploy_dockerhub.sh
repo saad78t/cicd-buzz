4907f77baf3a8fa8d82904cec0743fda80e9ab4d
N8470 <n8470@student.jamk.fi>
2020-04-18 14:41:35 +0300

    modifying

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -5,5 +5,6 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t $DOCKER_PATH=$DOCKER_USER/$DOCKERHUB_REPO:$TAG .
-docker push $DOCKER_PATH=$DOCKER_USER/$DOCKERHUB_REPO:$TAG
+DOCKER_PATH=$DOCKER_USER/$DOCKERHUB_REPO:$TAG
+docker build -f Dockerfile -t $DOCKER_PATH .
+docker push $DOCKER_PATH
